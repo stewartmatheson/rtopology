1688f24b8107f11166c1d290cb3ee4164ed7f0c9
Stewart Matheson <stew@rtmatheson.com>
2012-10-28 15:32:59 +1100

    Added more fields to pages

--- db/migrate/20120809082321_create_pages.rb
+++ db/migrate/20120809082321_create_pages.rb
@@ -4,6 +4,11 @@ class CreatePages < ActiveRecord::Migration
       t.text :path
       t.integer :site_id
       t.integer :discovered_id
+      t.integer :response_time
+      t.integer :response_code
+      t.datetime :last_audited_at
+      t.string :last_error
+      t.datetime :last_error_at
 
       t.timestamps
     end
